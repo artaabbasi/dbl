fc54893b497a002adfbf90eb21b8e2aa515051cb
Arta Abbasi <arta1382@gmail.com>
2025-10-18 12:02:09 +0330

    Update ex-01.sql and add 4 query

--- ex-01.sql
+++ ex-01.sql
@@ -7,8 +7,12 @@ FROM SalesLT.CustomerAddress ca
 LEFT JOIN SalesLT.Address a ON ca.AddressID = a.AddressID
 GROUP BY a.City;
 
-
 SELECT so.SalesOrderID, so.OrderDate, COUNT(sod.ProductID) AS ItemCount
 FROM SalesLT.SalesOrderHeader AS so
 JOIN SalesLT.SalesOrderDetail AS sod ON so.SalesOrderID = sod.SalesOrderID
 GROUP BY so.SalesOrderID, so.OrderDate;
+
+SELECT TOP 10 c.CustomerID, MAX(so.SubTotal)
+WHERE SalesLT.SalesOrderHeader AS so
+JOIN Customer AS c ON so.CustomerID = c.Customer 
+GROUP BY c.CustomerID;
