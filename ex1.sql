744a721252217dff4044885681c53af8024dfdcd
Arta Abbasi <arta1382@gmail.com>
2025-10-18 11:44:07 +0330

    ex1.sql

---
+++ ex1.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM SalesLT.Customer 
+WHERE LOWER(RIGHT(FirstName, 1)) IN ('a', 'b', 'c', 'd', 'e', 'f');
+
+SELECT a.City, COUNT(ca.CustomerID)
+FROM SalesLT.CustomerAddress ca
+LEFT JOIN SalesLT.Address a ON ca.AddressID = a.AddressID
+GROUP BY a.City;
+
+
+SELECT so.SalesOrderID, so.OrderDate, COUNT(sod.ProductID) AS ItemCount
+FROM SalesLT.SalesOrderHeader AS so
+JOIN SalesLT.SalesOrderDetail AS sod ON so.SalesOrderID = sod.SalesOrderID
+GROUP BY so.SalesOrderID, so.OrderDate;
